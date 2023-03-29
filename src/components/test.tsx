08ccb932cfebd31bda9d8d6f9035058cd90e7f03
Shouhei Yamaguchi <shouheiyamaguchi@Shoheis-MacBook-Pro.local>
2023-03-29 18:03:05 +0900

    環境変数に関する設定

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -5,8 +5,23 @@ type TestPropsType = {
 };
 
 const Test: React.FC<TestPropsType> = ({ text }) => {
+	const handleClickFetch = async () => {
+		const res = await fetch(
+			'https://api.twitch.tv/helix/games?name=elden ring',
+			{
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Client-ID': String(import.meta.env.VITE_TWITCH_CLIENT_ID),
+					Authorization: `Bearer ${import.meta.env.VITE_TWITCH_TOKEN}`,
+				},
+			}
+		);
+	};
+
 	return (
 		<>
+			<button onClick={handleClickFetch}>Fetch</button>
 			<p className={styles.test__text}>{text}</p>
 		</>
 	);
