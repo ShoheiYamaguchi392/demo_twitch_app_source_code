2f9b42d4f65f2e84b337f8844f751eacf6d06fb1
Shohei Yamaguchi <skytransform392@gmail.com>
2023-04-01 12:54:09 +0900

    [WIP]共通のレイアウト実装

--- src/components/Layout/MainLayout/index.tsx
+++ src/components/Layout/MainLayout/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import styles from './MainLayout.module.scss';
 
-const MainLayout = () => {
-	return <div className={styles.wrapper}>MainLayout</div>;
+import Header from '@/components/Moleculus/Header';
+
+type MainLayoutType = {
+	children?: ReactNode;
+};
+
+const MainLayout: React.FC<MainLayoutType> = ({ children }) => {
+	return (
+		<div className={styles.wrapper}>
+			<Header />
+			<div className={styles['main-content-wrapper']}>children</div>
+		</div>
+	);
 };
 
 export default MainLayout;
